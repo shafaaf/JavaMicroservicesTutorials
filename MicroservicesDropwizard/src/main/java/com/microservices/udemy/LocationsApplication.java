cfaaf8cdf616ebca66be4b27641d5f05c8bdb808
Shafaaf Hossain <EL-CAPITAN-2016@Mackintoshs-MacBook-Pro-3.local>
2019-11-27 00:42:04 -0500

    have basic dropqizard hello world

---
+++ MicroservicesDropwizard/src/main/java/com/microservices/udemy/LocationsApplication.java
@@ -0,0 +1,17 @@
+package com.microservices.udemy;
+
+import io.dropwizard.Application;
+import io.dropwizard.Configuration;
+import io.dropwizard.setup.Environment;
+
+public class LocationsApplication extends Application<Configuration> {
+
+    @Override
+    public void run(Configuration configuration, Environment environment) throws Exception {
+        System.out.println("Hello world");
+    }
+
+    public static void main(String[] args) throws Exception {
+        new LocationsApplication().run(args);
+    }
+}
---
+++ MicroservicesDropwizard/src/main/java/com/microservices/udemy/model/BaseEntity.java
@@ -0,0 +1,38 @@
+package com.microservices.udemy.model;
+
+public class BaseEntity {
+
+    private Long id;
+    private String createdOn;
+    private String modifiedOn;
+
+    public BaseEntity(Long id, String createdOn, String modifiedOn) {
+        this.id = id;
+        this.createdOn = createdOn;
+        this.modifiedOn = modifiedOn;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(String createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public String getModifiedOn() {
+        return modifiedOn;
+    }
+
+    public void setModifiedOn(String modifiedOn) {
+        this.modifiedOn = modifiedOn;
+    }
+}
