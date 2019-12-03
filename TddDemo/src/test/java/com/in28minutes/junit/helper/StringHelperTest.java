621f8107a827b51729a80c5070140dc2a97f28ef
Shafaaf Hossain <EL-CAPITAN-2016@Mackintoshs-MacBook-Pro-3.local>
2019-12-02 23:42:49 -0500

    doing TddDemo from Udemy

---
+++ TddDemo/src/test/java/com/in28minutes/junit/helper/StringHelperTest.java
@@ -0,0 +1,18 @@
+package com.in28minutes.junit.helper;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StringHelperTest {
+
+    @Test
+    public void truncateAInFirst2Positions() {
+        System.out.println("truncateAInFirst2Positions");
+    }
+
+    @Test
+    public void areFirstAndLastTwoCharactersTheSame() {
+        System.out.println("areFirstAndLastTwoCharactersTheSame");
+    }
+}
