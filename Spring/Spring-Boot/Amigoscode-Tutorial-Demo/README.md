6892be10a194bf9eb5451baa619b2dd725ea727e
shafaaf <shafaaf.hossain@hotmail.com>
2022-11-11 03:00:05 -0500

    This is a new change

---
+++ Spring/Spring-Boot/Amigoscode-Tutorial-Demo/README.md
@@ -0,0 +1,3 @@
+http://localhost:8080/
+
+http://localhost:8080/api/v1/student
---
+++ Spring/Spring-Boot/Amigoscode-Tutorial-Demo/src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,40 @@
+package com.example.demo.student;
+
+import java.time.LocalDate;
+
+public class Student {
+    public Long id;
+    public String name;
+    public String email;
+    public LocalDate dob;
+    public Integer age;
+
+    public Student() {
+    }
+
+    public Student(Long id, String name, String email, LocalDate dob, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.dob = dob;
+        this.age = age;
+    }
+
+    public Student(String name, String email, LocalDate dob, Integer age) {
+        this.name = name;
+        this.email = email;
+        this.dob = dob;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", dob=" + dob +
+                ", age=" + age +
+                '}';
+    }
+}
