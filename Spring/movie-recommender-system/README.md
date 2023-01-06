51d80f4a5af1958bf134a11a1c1937794158e93f
shafaaf <shafaaf.hossain@hotmail.com>
2023-01-06 17:19:44 -0500

    i

---
+++ Spring/movie-recommender-system/README.md
@@ -0,0 +1,3 @@
+## Movie-recommender-system
+
+Done from: https://www.educative.io/courses/guide-spring-5-spring-boot-2
--- Spring/spring-basic-1/src/main/java/com/spring/basics/springdemo/SortAlgorithm.java
+++
@@ -1,5 +0,0 @@
-package com.spring.basics.springdemo;
-
-public interface SortAlgorithm {
-    int[] sort(int[] numbers);
-}
